e4654eb3b841ecfe80def39b406da8f091f3a276
WllY23 <arias1533@gmail.com>
2018-02-02 20:18:25 -0500

    script de respawn
    inserte el respawn para q al caer llegue a ´punto de salida

---
+++ ejemplogithub/Assets/Script/respawn.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class respawn : MonoBehaviour
+{
+    public GameObject Cube;
+    public float limite;
+    public Vector3 puntosalida;
+    void Update()
+    {
+        if (Cube.transform.position.y < limite)
+        {
+            Cube.transform.position = puntosalida;
+        }
+    }
+}
+
+
